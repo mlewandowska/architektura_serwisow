017068b9243a1315f6c8aaf533fab8fe43ba04f1
Marta Lewandowska <mlewandowska@sigma.inf.ug.edu.pl>
2012-11-12 01:21:34 +0100

    blogowe kombinacje

---
+++ blog/app/models/post.rb
@@ -0,0 +1,4 @@
+class Post < ActiveRecord::Base
+  attr_accessible :text, :title
+has_many :comments	
+end
---
+++ blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ebfbf4fd3a0e16244f1390beebc8e44a50540d61d03c8493929b4d675381ecdb133f37f1bb97961177592643703b38cd3624bb774d4158f0d8dd456ddb1d8881'
